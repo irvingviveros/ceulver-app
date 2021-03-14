2eb0fc4ba6ca06e636014e6f7659d312b857e22d
irvingviveros <irvingviveros@users.noreply.github.com>
2021-03-13 21:59:47 -0600

    Initial commit

---
+++ app/Http/Controllers/LanguageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class LanguageController extends Controller
+{
+    //
+    public function swap($locale){
+        // available language in template array
+        $availLocale=['en'=>'en'];
+        // check for existing language
+        if(array_key_exists($locale,$availLocale)){
+            session()->put('locale',$locale);
+        }
+        return redirect()->back();
+    }
+}
