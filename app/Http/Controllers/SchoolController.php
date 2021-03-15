cfd62bbb527f141f428b5dfa355ee15e0a24ed9a
irvingviveros <irvingviveros@users.noreply.github.com>
2021-03-15 12:05:53 -0600

    Creation of School model with controller and finished Role Seeder info

---
+++ app/Http/Controllers/SchoolController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SchoolController extends Controller
+{
+    //
+}
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,16 +16,114 @@ class RoleSeeder extends Seeder
     public function run()
     {
         DB::table('roles')->insert([
-            'slug' => 'super-admin',
-            'name' => 'Super Admin',
-            'note' => 'Usuario Super Administrador',
-            'is_default' => 1,
-            'is_super_admin' => 1,
-            'status' => 1,
-            'created_by' => 0,
-            'modified_by' => 1,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            [
+                'slug' => 'super-admin',
+                'name' => 'Super Admin',
+                'note' => 'Usuario Super Administrador',
+                'is_default' => 1,
+                'is_super_admin' => 1,
+                'status' => 1,
+                'created_by' => 0,
+                'modified_by' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'slug' => 'admin',
+                'name' => 'Admin',
+                'note' => 'Usuario Administrador',
+                'is_default' => 1,
+                'is_super_admin' => 0,
+                'status' => 1,
+                'created_by' => 0,
+                'modified_by' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'slug' => 'tutor',
+                'name' => 'Tutor',
+                'note' => 'Padre o Tutor',
+                'is_default' => 1,
+                'is_super_admin' => 0,
+                'status' => 1,
+                'created_by' => 0,
+                'modified_by' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'slug' => 'estudiante',
+                'name' => 'Estudiante',
+                'note' => 'Estudiante',
+                'is_default' => 1,
+                'is_super_admin' => 0,
+                'status' => 1,
+                'created_by' => 0,
+                'modified_by' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'slug' => 'profesor',
+                'name' => 'Profesor',
+                'note' => 'Profesor',
+                'is_default' => 1,
+                'is_super_admin' => 0,
+                'status' => 1,
+                'created_by' => 0,
+                'modified_by' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'slug' => 'financiero',
+                'name' => 'Financiero',
+                'note' => 'Departamento Financiero',
+                'is_default' => 1,
+                'is_super_admin' => 0,
+                'status' => 1,
+                'created_by' => 0,
+                'modified_by' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'slug' => 'bibliotecario',
+                'name' => 'Bibliotecario',
+                'note' => 'Bibliotecario',
+                'is_default' => 1,
+                'is_super_admin' => 0,
+                'status' => 1,
+                'created_by' => 0,
+                'modified_by' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'slug' => 'recepcionista',
+                'name' => 'Recepcionista',
+                'note' => 'Recepcionista / Usuario de receipción',
+                'is_default' => 1,
+                'is_super_admin' => 0,
+                'status' => 1,
+                'created_by' => 0,
+                'modified_by' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'slug' => 'staff',
+                'name' => 'Staff',
+                'note' => 'Usuario de personal staff',
+                'is_default' => 0,
+                'is_super_admin' => 0,
+                'status' => 1,
+                'created_by' => 0,
+                'modified_by' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]
         ]);
     }
 }
