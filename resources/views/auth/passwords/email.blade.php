a1e5d198d6e0a5d2db8e2be7aa51214d489cb5d5
irvingviveros <irvingviveros@users.noreply.github.com>
2021-08-30 15:11:45 -0500

    Reset email password template updated for Bootstrap 5

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,22 +3,23 @@
 @endphp
 @extends('layouts/fullLayoutMaster')
 
-@section('title', 'Cambio de NIP')
+@section('title', 'Reestablecer NIP')
 
 @section('page-style')
   {{-- Page Css files --}}
   <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
-  <link rel="stylesheet" href="{{ asset(mix('css/base/pages/page-auth.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('css/base/pages/authentication.css')) }}">
 @endsection
 
 @section('content')
-  <div class="auth-wrapper auth-v2">
+  <div class="auth-wrapper auth-cover">
     <div class="auth-inner row m-0">
       <!-- Brand logo-->
-      <a class="brand-logo" href="javascript:void(0);">
+      <a class="brand-logo" href="/">
         <img src="{{asset('/images/logo/ceulver_transparent.png')}}" alt="Universidad CEULVER">
       </a>
       <!-- /Brand logo-->
+
       <!-- Left Text-->
       <div class="d-none d-lg-flex col-lg-8 align-items-center p-5">
         <div class="w-100 d-lg-flex align-items-center justify-content-center px-5">
@@ -26,23 +27,26 @@
         </div>
       </div>
       <!-- /Left Text-->
+
       <!-- Forgot password-->
       <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
         <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
-          <h2 class="card-title font-weight-bold mb-1">¿Olvidaste tu NIP?</h2>
-          <p class="card-text mb-2">Ingresa tu correo electrónico personal y te enviaremos instrucciones para restablecer tu NIP.</p>
+          <h2 class="card-title fw-bold mb-1">¿Olvidaste tu NIP?</h2>
+          <p class="card-text mb-2">Ingresa tu correo electrónico personal y te enviaremos instrucciones para restablecer tu NIP</p>
           <form class="auth-forgot-password-form mt-2" method="POST" action="{{ route('password.email') }}">
             @csrf
-            <div class="form-group">
-              <label for="forgot-password-email" class="form-label">Correo electrónico</label>
-              <input type="text" class="form-control @error('email') is-invalid @enderror" id="forgot-password-email" name="email" value="{{ old('email') }}" placeholder="john@example.com" aria-describedby="forgot-password-email" tabindex="1" autofocus />
+            <div class="mb-1">
+              <label class="form-label" for="forgot-password-email">Correo electrónico</label>
+              <input type="text" class="form-control @error('email') is-invalid @enderror" id="forgot-password-email"
+                     name="email" value="{{ old('email') }}" placeholder="john@example.com"
+                     aria-describedby="forgot-password-email" tabindex="1" autofocus required/>
               @error('email')
               <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
               </span>
               @enderror
             </div>
-            <button class="btn btn-primary btn-block" tabindex="2">Restablecer NIP</button>
+            <button class="btn btn-primary w-100" tabindex="2">Enviar instrucciones</button>
           </form>
           <p class="text-center mt-2">
             @if (Route::has('login'))
@@ -61,5 +65,5 @@
 @endsection
 
 @section('page-script')
-  <script src="{{asset(mix('js/scripts/pages/page-auth-forgot-password.js'))}}"></script>
+  <script src="{{asset(mix('js/scripts/pages/auth-forgot-password.js'))}}"></script>
 @endsection
