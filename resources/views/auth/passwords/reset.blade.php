2b0f2348b261e476f18e5fd4d6d2b994c5154883
irvingviveros <irvingviveros@users.noreply.github.com>
2021-09-20 10:51:43 -0500

    Text updated

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,43 +1,32 @@
-@php
-  $configData = Helper::applClasses();
-@endphp
 @extends('layouts/fullLayoutMaster')
 
 @section('title', 'Reset Password')
 
 @section('page-style')
   {{-- Page Css files --}}
-  <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
-  <link rel="stylesheet" href="{{ asset(mix('css/base/pages/page-auth.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('css/base/pages/authentication.css')) }}">
 @endsection
 
 @section('content')
-  <div class="auth-wrapper auth-v2">
-    <div class="auth-inner row m-0">
-      <!-- Brand logo-->
-      <a class="brand-logo" href="javascript:void(0);">
-        <img src="{{asset('/images/logo/ceulver_transparent.png')}}" alt="Universidad CEULVER">
-      </a>
-      <!-- /Brand logo-->
-      <!-- Left Text-->
-      <div class="d-none d-lg-flex col-lg-8 align-items-center p-5">
-        <div class="w-100 d-lg-flex align-items-center justify-content-center px-5">
-          <img src="{{asset('images/pages/reset-password-v2.svg')}}" class="img-fluid" alt="Restaurar NIP" />
-        </div>
-      </div>
-      <!-- /Left Text-->
-      <!-- Reset password-->
-      <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
-        <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
-          <h2 class="card-title font-weight-bold mb-1">Restablecer el NIP</h2>
-          <p class="card-text mb-2">Su nuevo NIP debe ser diferente de los utilizados anteriormente.</p>
+  <div class="auth-wrapper auth-basic px-2">
+    <div class="auth-inner my-2">
+      <!-- Reset Password basic -->
+      <div class="card mb-0">
+        <div class="card-body">
+          <a href="javascript:void(0);" class="brand-logo">
+            <h2 class="brand-text text-primary ms-1">CEULVER</h2>
+          </a>
+
+          <h4 class="card-title mb-1">Reestablecer contraseña</h4>
+          <p class="card-text mb-2">Su nueva contraseña debe ser diferente de las contraseñas utilizadas anteriormente</p>
+
           <form class="auth-reset-password-form mt-2" method="POST" action="{{ route('password.update') }}">
             @csrf
             <input type="hidden" name="token" value="{{ $token }}">
 
-            <div class="form-group">
+            <div class="mb-1">
               <label for="email" class="form-label">Correo electrónico</label>
-              <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="john@example.com" aria-describedby="email" tabindex="1" autofocus value="{{ $email ?? old('email') }}" disabled/>
+              <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="john@example.com" aria-describedby="email" tabindex="1" autofocus value="{{ $email ?? old('email') }}"/>
               @error('email')
               <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -45,15 +34,13 @@
               @enderror
             </div>
 
-            <div class="form-group">
+            <div class="mb-1">
               <div class="d-flex justify-content-between">
                 <label for="reset-password-new">Nueva contraseña</label>
               </div>
               <div class="input-group input-group-merge form-password-toggle @error('password') is-invalid @enderror">
                 <input type="password" class="form-control form-control-merge @error('password') is-invalid @enderror" id="reset-password-new" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="reset-password-new" tabindex="2" autofocus />
-                <div class="input-group-append">
-                  <span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span>
-                </div>
+                <span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span>
               </div>
               @error('password')
               <span class="invalid-feedback" role="alert">
@@ -61,38 +48,28 @@
               </span>
               @enderror
             </div>
-            <div class="form-group">
+            <div class="mb-1">
               <div class="d-flex justify-content-between">
                 <label for="reset-password-confirm">Confirmar contraseña</label>
               </div>
               <div class="input-group input-group-merge form-password-toggle">
                 <input type="password" class="form-control form-control-merge" id="reset-password-confirm" name="password_confirmation" autocomplete="new-password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="reset-password-confirm" tabindex="3" />
-                <div class="input-group-append">
-                  <span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span>
-                </div>
+                <span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span>
               </div>
             </div>
-            <button type="submit" class="btn btn-primary btn-block" tabindex="4">Actualizar contraseña</button>
+            <button type="submit" class="btn btn-primary w-100" tabindex="4">Establecer nueva contraseña</button>
           </form>
 
           <p class="text-center mt-2">
             @if (Route::has('login'))
               <a href="{{ route('login') }}">
-                <i data-feather="chevron-left"></i> Regresar a inicio de sesión
+                <i data-feather="chevron-left"></i> Regresar a inicio
               </a>
             @endif
           </p>
         </div>
       </div>
-      <!-- /Reset password-->
+      <!-- /Reset Password basic -->
     </div>
   </div>
-@endsection
-
-@section('vendor-script')
-  <script src="{{asset(mix('vendors/js/forms/validation/jquery.validate.min.js'))}}"></script>
-@endsection
-
-@section('page-script')
-  <script src="{{asset(mix('js/scripts/pages/page-auth-reset-password.js'))}}"></script>
 @endsection
