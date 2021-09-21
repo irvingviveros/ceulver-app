54076f5e18f278cba49e54345504a983a716ed22
irvingviveros <irvingviveros@users.noreply.github.com>
2021-09-21 10:16:54 -0500

    Renamed from StudentType to StudentConvention

--- database/seeders/StudentConvention.php
+++ database/seeders/StudentConvention.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class StudentType extends Seeder
+class StudentConvention extends Seeder
 {
     /**
      * Run the database seeds.
