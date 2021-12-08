42203111f7fddb6125bba44345e081b407901f34
irvingviveros <irvingviveros@users.noreply.github.com>
2021-12-07 20:18:18 -0600

    Developers can create factories and attach to seeders

--- src/App/Providers/AppServiceProvider.php
+++ src/App/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Factory::guessFactoryNamesUsing(function ($class) {
+            return 'Database\\Factories\\' . class_basename($class) . 'Factory';
+        });
     }
 
     /**
