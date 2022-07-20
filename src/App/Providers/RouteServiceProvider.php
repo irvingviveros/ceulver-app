17b3cf0b5a5c092d07c5443001756f90faffce22
irvingviveros <irvingviveros@users.noreply.github.com>
2022-07-20 00:04:21 -0500

    Updated packages, start developing new module

--- src/App/Providers/RouteServiceProvider.php
+++ src/App/Providers/RouteServiceProvider.php
@@ -17,7 +17,16 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
+
+    /**
+     * The controller namespace for the application.
+     *
+     * When present, controller route declarations will automatically be prefixed with this namespace.
+     *
+     * @var string|null
+     */
+    // protected $namespace = 'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
