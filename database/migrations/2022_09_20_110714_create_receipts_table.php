d55dd49ef0a9c6a9fe7237c29fd8154a35abdef0
irvingviveros <irvingviveros@users.noreply.github.com>
2022-10-02 13:05:02 -0500

    feat: creating receipt base model and migration

---
+++ database/migrations/2022_09_20_110714_create_receipts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReceiptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receipts', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference')->nullable(false);
+            $table->string('payment_method')->nullable(false);
+            $table->string('payment_concept')->nullable(false);
+            $table->decimal('amount', 10, 2)->nullable(false);
+            $table->string('amount_text')->nullable(false);
+//            $table->string('status')->nullable();
+            $table->date('issued_date')->nullable();
+            $table->date('note')->nullable();
+            $table->integer('created_by');
+            $table->integer('modified_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receipts');
+    }
+}
--- src/Infrastructure/Receipt/Model/Receipt.php
+++ src/Infrastructure/Receipt/Model/Receipt.php
@@ -3,17 +3,16 @@ declare(strict_types = 1);
 
 namespace Infrastructure\Receipt\Model;
 
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Infrastructure\Student\Model\Student;
 
 class Receipt extends Model
 {
     use HasFactory;
 
-    public function student()
-    {
-        return $this->belongsTo(Student::class, 'student_id');
-    }
+    /**
+     * Get the studemt associated with the student.
+     *  $student -> receipts
+     */
+
 }
