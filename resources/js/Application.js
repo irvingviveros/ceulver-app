8821e043a66e0fcb198763d099817cbb2ec7d86b
irvingviveros <irvingviveros@users.noreply.github.com>
2022-10-03 17:40:46 -0500

    style: added new lines

--- resources/js/Application.js
+++ resources/js/Application.js
@@ -32,11 +32,12 @@ const Application = (function () {
                     }
                 ],
                 dom: '<"card-header border-bottom p-1"<"head-label"><"dt-action-buttons text-end"B>><"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
-                buttons: [{
-                    extend: 'colvis',
-                    className: 'btn btn-outline-secondary dropdown-toggle me-2',
-                    columns: ':not(.noVis)'
-                },
+                buttons: [
+                    {
+                        extend: 'colvis',
+                        className: 'btn btn-outline-secondary dropdown-toggle me-2',
+                        columns: ':not(.noVis)'
+                    },
                     {
                         extend: 'collection',
                         className: 'btn btn-outline-secondary dropdown-toggle me-2',
