1bc9ec77f0512e201c62343010b6df7d9641efb8
irvingviveros <irvingviveros@users.noreply.github.com>
2022-10-11 14:58:18 -0500

    refactor: clear username field according to the selected school system.

--- resources/js/utils/student-username-generator.js
+++ resources/js/utils/student-username-generator.js
@@ -14,12 +14,29 @@ $('#nationalId').blur(function() {
     // Check if the national id is less than 5 characters. If this is the case, it clears the username field.
     if (nationalId.length < 5) {
         $('#studentUsername').val('')
+        $('#guardianUsername').val('')
     }
 
     // If the national id field has more than 5 characters, then generate the username
     if (nationalId.length >= 5) {
         let username = nationalId.slice(0,5); // Slice the national id and saves the first 5 characters
-        $('#studentUsername').val(username + currentYear + currentMonth);
-        $('#guardianUsername').val(username + currentYear + currentMonth);
+
+        $("#schoolSelect").change(function (){
+            let selector = $('div[dynamic-toggle]');
+            let option = $('option:checked', this).attr('educationalSystem')
+            option === $('div[data-system]').data('system') ? selector.removeClass("d-none") : selector.addClass("d-none")
+
+            // If value from select equals 'Universidad', hide divs with class 'hide-toggle'
+            if (option === 'Universidad') {
+                $('#studentUsername').val(username + currentYear + currentMonth);
+                $('#guardianUsername').val('')
+                console.log('Usuario del padre eliminado ' + $('#guardianUsername').val(''))
+            } else {
+                $('#guardianUsername').val(username + currentYear + currentMonth);
+                $('#studentUsername').val('')
+                console.log('Usuario del alumno eliminado ' + $('#studentUsername').val(''))
+            }
+
+        })
     }
 });
