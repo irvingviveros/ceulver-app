87443be173532cec31025153d8eff7a8c758893e
irvingviveros <irvingviveros@users.noreply.github.com>
2022-10-11 15:51:33 -0500

    refactor: updated student migration

--- database/migrations/2021_09_20_134253_create_students_table.php
+++ database/migrations/2021_09_20_134253_create_students_table.php
@@ -25,22 +25,21 @@ class CreateStudentsTable extends Migration
             $table->string('first_name', 35);  // Nombre(s)
             $table->string('paternal_surname', 25)->nullable(true); // Apellido paterno
             $table->string('maternal_surname', 25)->nullable(true); // Apellido materno
-            $table->date('birth_date'); // Fecha cumpleaños
+            $table->date('birth_date')->nullable(false); // Fecha cumpleaños
             $table->integer('age')->nullable(true); // Edad //TODO: calcular automáticamente edad en base a birth_date
             $table->string('occupation', 100)->nullable(true); // Ocupación
-            $table->string('address')->nullable(true); // Dirección
+            $table->string('address', 250)->nullable(false); // Dirección
             $table->string('personal_email')->unique()->nullable(true); // Correo personal
-            $table->string('home_phone', 10)->nullable(true); // Teléfono de casa
             $table->string('personal_phone', 17)->nullable(true); // Teléfono personal
-            $table->string('nationality', 25)->default('Mexicano'); // Nacionalidad
-            $table->string('marital_status')->nullable(true)->default('No especifica'); //Estado civil
+            $table->string('marital_status')->nullable(true)->default('No aplica'); //Estado civil
             $table->string('sex', 20)->nullable(true); // SexEnum, Sexo
             // Health and other info
             $table->string('blood_group', 15)->nullable(true); // Grupo de sangre
-            $table->string('allergies', 100)->nullable(true); // Alergias
-            $table->string('ailments', 100)->nullable(true); // Padecimientos
+            $table->string('allergies', 250)->nullable(true); // Alergias
+            $table->string('ailments', 250)->nullable(true); // Padecimientos
             $table->text('other_info')->nullable(true); // Otra información, notas
             $table->text('health_condition')->nullable(true); // Condición de salud
+            $table->string('guardian_relationship')->nullable(false)->default('Otro'); // Relación con el padre/tutor
             $table->timestamps();
 
             // Foreign key
