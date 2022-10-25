154c95dc4a6f9b4c53390ca3f5e07e674ec88159
irvingviveros <irvingviveros@users.noreply.github.com>
2022-10-25 18:34:35 -0500

    fix: prevent to send information before checking if the data is correct

--- resources/js/scripts/tables/modules/student/student-datatables.js
+++ resources/js/scripts/tables/modules/student/student-datatables.js
@@ -86,53 +86,56 @@ const StudentDatatable = (function () {
                 AppNotification.show(
                     'warning', 'Hubo un error al intentar crear el registro.', 'Advertencia'
                 );
-            }
-            // This is sent to the store request parameter
-            StudentDatatable.save({
-                _token: Application.getToken()
-                , school_id: form.find('select[id="schoolSelect"]').val()
-                , paternal_surname: form.find('input[id="paternalSurname"]').val()
-                , maternal_surname: form.find('input[id="maternalSurname"]').val()
-                , first_name: form.find('input[id="firstName"]').val()
-                , birth_date: form.find('input[id="birthday"]').val()
-                , national_id: form.find('input[id="nationalId"]').val()
-                , address: form.find('input[id="address"]').val()
-                , occupation: form.find('input[id="occupation"]').val()
-                , sex: form.find('select[id="sexSelect"]').val()
-                , marital_status: form.find('select[id="maritalStatus"]').val()
-                , email: form.find('input[id="email"]').val()
-                , phone: form.find('input[id="phone"]').val()
-                , blood_group: form.find('select[id="bloodGroup"]').val()
-                , ailments: form.find('input[id="ailments"]').val()
-                , allergies: form.find('input[id="allergies"]').val()
-                , career: form.find('select[id="careerSelect"]').val()
-                , enrollment: form.find('input[id="enrollment"]').val()
-                , payment_reference: form.find('input[id="paymentReference"]').val()
-                , guardian_last_name: form.find('input[id="guardianLastName"]').val()
-                , guardian_first_name: form.find('input[id="guardianFirstName"]').val()
-                , guardian_relationship: form.find('select[id="guardianRelationship"]').val()
-                , guardian_address: form.find('input[id="guardianAddress"]').val()
-                , guardian_email: form.find('input[id="guardianEmail"]').val()
-                , guardian_phone: form.find('input[id="guardianPhone"]').val()
-                , guardian_username: form.find('input[id="guardianUsername"]').val()
-                , guardian_password: form.find('input[id="guardianPassword"]').val()
-                , student_username: form.find('input[id="studentUsername"]').val()
-                , student_password: form.find('input[id="studentPassword"]').val()
-                , student_status: form.find('select[id="studentStatus"]').val()
-                , educational_system: $('option:checked', form.find('select[id="schoolSelect"]')).attr('educationalSystem')
-            }).then(function () {
-                AppNotification.show(
-                    'success', 'El registro ha sido creado correctamente', 'Registro creado'
-                );
+            } else {
+                // This is sent to the store request parameter
+                StudentDatatable.save({
+                    _token: Application.getToken()
+                    , school_id: form.find('select[id="schoolSelect"]').val()
+                    , paternal_surname: form.find('input[id="paternalSurname"]').val()
+                    , maternal_surname: form.find('input[id="maternalSurname"]').val()
+                    , first_name: form.find('input[id="firstName"]').val()
+                    , birth_date: form.find('input[id="birthday"]').val()
+                    , national_id: form.find('input[id="nationalId"]').val()
+                    , address: form.find('input[id="address"]').val()
+                    , occupation: form.find('input[id="occupation"]').val()
+                    , sex: form.find('select[id="sexSelect"]').val()
+                    , marital_status: form.find('select[id="maritalStatus"]').val()
+                    , email: form.find('input[id="email"]').val()
+                    , phone: form.find('input[id="phone"]').val()
+                    , blood_group: form.find('select[id="bloodGroup"]').val()
+                    , ailments: form.find('input[id="ailments"]').val()
+                    , allergies: form.find('input[id="allergies"]').val()
+                    , career: form.find('select[id="careerSelect"]').val()
+                    , enrollment: form.find('input[id="enrollment"]').val()
+                    , payment_reference: form.find('input[id="paymentReference"]').val()
+                    , guardian_last_name: form.find('input[id="guardianLastName"]').val()
+                    , guardian_first_name: form.find('input[id="guardianFirstName"]').val()
+                    , guardian_relationship: form.find('select[id="guardianRelationship"]').val()
+                    , guardian_address: form.find('input[id="guardianAddress"]').val()
+                    , guardian_email: form.find('input[id="guardianEmail"]').val()
+                    , guardian_phone: form.find('input[id="guardianPhone"]').val()
+                    , guardian_username: form.find('input[id="guardianUsername"]').val()
+                    , guardian_password: form.find('input[id="guardianPassword"]').val()
+                    , student_username: form.find('input[id="studentUsername"]').val()
+                    , student_password: form.find('input[id="studentPassword"]').val()
+                    , student_status: form.find('select[id="studentStatus"]').val()
+                    , educational_system: $('option:checked', form.find('select[id="schoolSelect"]')).attr('educationalSystem')
+                }).then(function () {
+                    AppNotification.show(
+                        'success', 'El registro ha sido creado correctamente', 'Registro creado'
+                    );
 
-                Modal.close(modal.attr('id'));
-                // Reload table
-                StudentDatatable.getList().then(function () {
-                    $('table[id="dataTable"]').DataTable().destroy;
-                    $('div[id="dataList"]').html(arguments[0]);
-                    initializeTable();
+                    Modal.close(modal.attr('id'));
+                    // Reload table
+                    StudentDatatable.getList().then(function () {
+                        $('table[id="dataTable"]').DataTable().destroy;
+                        $('div[id="dataList"]').html(arguments[0]);
+                        initializeTable();
+                    });
                 });
-            });
+            }
+
+
         });
     }
 
