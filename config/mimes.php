33be40b90a3cd61bd1435f828c35d90046ae0621
irvingviveros <irvingviveros@users.noreply.github.com>
2022-11-16 12:49:54 -0600

    chore: added laravel admitted file extensions

---
+++ config/mimes.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+
+return array(
+
+    'csv' => array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream'),
+    'pdf' => array('application/pdf', 'application/x-download'),
+    'xls' => array('application/excel', 'application/vnd.ms-excel', 'application/msexcel'),
+    'ppt' => array('application/powerpoint', 'application/vnd.ms-powerpoint'),
+    'jpeg' => array('image/jpeg', 'image/pjpeg'),
+    'jpg' => array('image/jpeg', 'image/pjpeg'),
+    'jpe' => array('image/jpeg', 'image/pjpeg'),
+    'png' => 'image/png',
+    'txt' => 'text/plain',
+    'text' => 'text/plain',
+    'doc' => 'application/msword',
+    'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'word' => array('application/msword', 'application/octet-stream'),
+    'xl' => 'application/excel',
+
+);
