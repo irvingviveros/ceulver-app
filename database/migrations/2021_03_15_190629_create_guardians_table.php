4a70c304e69ad8a7183e2bf7985f64fbfdd07c38
irvingviveros <irvingviveros@users.noreply.github.com>
2022-11-23 18:34:32 -0600

    feat: removed guardian photo field in db

--- database/migrations/2021_03_15_190629_create_guardians_table.php
+++ database/migrations/2021_03_15_190629_create_guardians_table.php
@@ -16,11 +16,10 @@ class CreateGuardiansTable extends Migration
         Schema::create('guardians', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->string('last_name', 100)->nullable(false);
+            $table->string('last_name', 100);
             $table->string('phone', 10)->nullable(true);
             $table->string('email', 50)->nullable(true);
             $table->string('address', 255)->nullable(true);
-            $table->string('photo', 100)->nullable(true);
             $table->text('other_info')->nullable(true);
             $table->tinyInteger('status')->default(1);
             $table->integer('created_by');
@@ -28,7 +27,6 @@ class CreateGuardiansTable extends Migration
             $table->timestamps();
 
             // Foreign key
-            $table->foreignId('school_id')->constrained();
             $table->foreignId('user_id')->nullable();
         });
     }
