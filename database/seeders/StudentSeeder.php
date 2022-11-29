da4f814287a4958bf6537e99c8d2a1e63e974ed4
irvingviveros <irvingviveros@users.noreply.github.com>
2022-11-28 18:28:57 -0600

    feat: added career id to avoid an error

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Support\Enums\MaritalStatusEnum;
 
 class StudentSeeder extends Seeder
 {
@@ -16,30 +15,30 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        $date = now();   //Get current date
 
         DB::table('students')->insert([
-            'school_id' => 1,
-            'user_id' => 1, //TODO: Cambiar el user id cuando funcionen los permisos
+            'school_id'         => 1,
+            'user_id'           => 1, //TODO: Cambiar el user id cuando funcionen los permisos
             'payment_reference' => '2244242',
-            'national_id' => 'JAPJ970703HVZKLN07',
-            'enrollment' => 'IMA20220801',
-            'first_name' => 'Miguel',
-            'paternal_surname' => 'Pérez',
-            'maternal_surname' => 'Sánchez',
-            'birth_date' => Carbon::now()->subYears(16),
-            'age' => 16,
-            'sex' => 'Hombre',
-            'address' => 'Manuel Vazquez #38',
-            'occupation' => 'Estudiante',
-            'personal_email' => 'jake@gmail.com',
-            'personal_phone' => '2299533714',
-            'allergies' => 'Ninguna',
-            'ailments' => 'Ninguna',
-            'blood_group' => 'B+',
-            'marital_status' => 'Casado',
-            'created_by' => 1,
-            'created_at' => $date
+            'national_id'       => 'JAPJ970703HVZKLN07',
+            'enrollment'        => 'IMA20220801',
+            'first_name'        => 'Miguel',
+            'paternal_surname'  => 'Pérez',
+            'maternal_surname'  => 'Sánchez',
+            'birth_date'        => Carbon::now()->subYears(16),
+            'age'               => 16,
+            'sex'               => 'Hombre',
+            'address'           => 'Manuel Vazquez #38',
+            'occupation'        => 'Estudiante',
+            'personal_email'    => 'jake@gmail.com',
+            'personal_phone'    => '2299533714',
+            'allergies'         => 'Ninguna',
+            'ailments'          => 'Ninguna',
+            'blood_group'       => 'B+',
+            'marital_status'    => 'Casado',
+            'career_id'         => 5,
+            'created_by'        => 1,
+            'created_at'        => date_create()
         ]);
     }
 }
