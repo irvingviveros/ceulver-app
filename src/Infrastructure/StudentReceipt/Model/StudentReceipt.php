cbc4f1e16dfa924d4c1817dcb42f0ebd23663c27
irvingviveros <irvingviveros@users.noreply.github.com>
2022-11-30 13:28:59 -0600

    chore: created student receipt base model

---
+++ src/Infrastructure/StudentReceipt/Model/StudentReceipt.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Infrastructure\StudentReceipt\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Infrastructure\Receipt\Model\Receipt;
+use Infrastructure\Student\Model\Student;
+
+class StudentReceipt extends Model
+{
+    use HasFactory;
+
+    /**
+     * Get the student associated with the receipt.
+     *  $receiptStudent -> student
+     */
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    /**
+     * Get the receipt info associated with the student receipt.
+     *  $receiptStudent -> $receipt
+     */
+    public function receipt(): BelongsTo
+    {
+        return $this->belongsTo(Receipt::class);
+    }
+}
