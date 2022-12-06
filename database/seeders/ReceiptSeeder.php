9593343f89ed556cd1370de5bbbf7b5221916361
irvingviveros <irvingviveros@users.noreply.github.com>
2022-12-05 18:15:30 -0600

    chore: added receipt seeder data

---
+++ database/seeders/ReceiptSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Domain\Receipt\Service\ReceiptService;
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Infrastructure\Receipt\Repository\EloquentReceiptRepository;
+
+class ReceiptSeeder extends Seeder
+{
+    private ReceiptService $receiptService;
+
+    public function __construct()
+    {
+        $this->receiptService = new ReceiptService(
+            new EloquentReceiptRepository()
+        );
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($counter = 0; $counter <= 1380; $counter++) {
+
+            $dateTime = $faker->dateTimeBetween('-2 years');
+            $date = $dateTime->format('Y-m-d');
+
+            DB::table('receipts')->insert(
+                [
+                    'reference' => $faker->unique(true)->randomNumber(5, true),
+                    'sheet' => $counter,
+                    'payment_method' => $faker->randomElement(['Pago con tarjeta', 'Efectivo', 'Transferencia bancaria']),
+                    'payment_concept' => $faker->sentence(4, true),
+                    'amount' => $amount = $faker->randomNumber(4, false),
+                    'amount_text' => $this->receiptService->moneyToText($amount),
+                    'issued_date' => $date,
+                    'note' => $faker->paragraph(1),
+                    'created_by' => 1,
+                    'created_at' => now(),
+                    'modified_by' => null,
+                ]
+            );
+        }
+    }
+}
