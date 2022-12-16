4e4cbbc87104591378d62b05c66021439b6bc57a
irvingviveros <irvingviveros@users.noreply.github.com>
2022-12-16 14:15:26 -0600

    chore: added a method to get full name by paternal surname

--- src/Domain/Student/Actions/CreateFullName.php
+++ src/Domain/Student/Actions/CreateFullName.php
@@ -5,11 +5,19 @@ namespace Domain\Student\Actions;
 
 class CreateFullName
 {
-    public static function execute(
+    public static function ByFirstName(
         string $first_name,
         string $paternal_surname,
         ?string $maternal_surname): string
     {
         return "{$first_name} {$paternal_surname} {$maternal_surname}";
+    }
+
+    public static function ByPaternalSurname(
+        string $first_name,
+        string $paternal_surname,
+        ?string $maternal_surname): string
+    {
+        return "{$paternal_surname} {$maternal_surname} {$first_name}";
     }
 }
