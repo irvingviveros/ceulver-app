dc72981a089092db46cc1031fa5e0300e0c6fc4d
irvingviveros <irvingviveros@users.noreply.github.com>
2022-12-22 17:14:03 -0600

    chore: accounting general dashboard added

---
+++ resources/views/modules/accounting/receipts/index.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.contentLayoutMaster')
+
+@section('title', 'Administración de recibos')
+
+@section('vendor-style')
+    {{-- vendor css files --}}
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/dataTables.bootstrap5.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/responsive.bootstrap5.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/buttons.bootstrap5.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/rowGroup.bootstrap5.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/extensions/sweetalert2.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/extensions/toastr.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/pickers/flatpickr/flatpickr.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/feather-icons/feather-icons.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/animate/animate.min.css')) }}">
+@endsection
+@section('page-style')
+    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/pickers/form-flat-pickr.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/extensions/ext-component-toastr.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/extensions/ext-component-sweet-alerts.css')) }}">
+    <link rel="stylesheet" href="{{asset('css/base/pages/app-invoice.css')}}">
+@endsection
+
+@section('content')
+    <!-- Basic table -->
+    <section id="basic-datatable">
+        <div class="card" id="dataList">
+            <div class="card-body">
+                <div class="row">
+                    <div class="table-responsive">
+                        @if ($educationalSystemName === 'Universidad')
+                            @include('modules.accounting.receipts.list.university-list')
+                        @else
+                            @include('modules.accounting.receipts.list.list')
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!--/ Basic table -->
+@endsection
+
+@section('vendor-script')
+    {{-- vendor files --}}
+    <script src="{{ asset(mix('vendors/js/tables/datatable/jquery.dataTables.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.bootstrap5.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.responsive.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/responsive.bootstrap5.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.checkboxes.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.buttons.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/buttons.colVis.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/jszip.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/pdfmake.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/vfs_fonts.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/buttons.html5.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/buttons.print.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.rowGroup.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/extensions/sweetalert2.all.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/extensions/toastr.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/forms/validation/jquery.validate.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/pickers/flatpickr/flatpickr.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/extensions/moment.min.js')) }}"></script>
+
+@endsection
+@section('page-script')
+    <!-- Page JS files, global -->
+    <script src="{{ asset(mix('js/Application.js')) }}"></script>
+    <script src="{{ asset(mix('js/Modal.js')) }}"></script>
+    <script src="{{ asset(mix('js/Configuration.js')) }}"></script>
+    <script src="{{ asset(mix('js/AppNotification.js')) }}"></script>
+    <script src="{{ asset(mix('js/Delete.js')) }}"></script>
+    <!-- Page JS files, global -->
+
+    <!-- Local JS files -->
+
+    <!-- Datatable config, events and data -->
+
+    <script src="{{ asset(mix('data/datatables/student-receipts/student-receipts-datatable.js')) }}"></script>
+    <script
+        src="{{ asset(mix('js/scripts/tables/modules/accounting/student-receipts-events.js')) }}"
+        onload="StudentReceiptsEvents.load()">
+    </script>
+    <!-- Datatable config, events and data -->
+
+    <!-- Local JS files -->
+@endsection
