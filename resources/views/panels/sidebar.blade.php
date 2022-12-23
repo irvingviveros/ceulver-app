04551914dad6cd8c9069878695e17518e1e3848e
irvingviveros <irvingviveros@users.noreply.github.com>
2022-12-22 20:07:20 -0600

    refactor: removed unnecessary statement

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -42,25 +42,24 @@
                         @endphp
                         @if($menu->name === 'Administrator' && auth()->user()->cannot('see super-admin panel'))
                             @continue
-                        @else
-                            <li
-                                class="nav-item {{ $custom_classes }} {{ Route::currentRouteName() === $menu->slug ? 'active' : '' }}">
-                                <a href="{{ isset($menu->url) ? url($menu->url) : 'javascript:void(0)' }}"
-                                   class="d-flex align-items-center"
-                                   target="{{ isset($menu->newTab) ? '_blank' : '_self' }}">
-                                    <i data-feather="{{ $menu->icon }}"></i>
-                                    <span class="menu-title text-truncate">{{ __('locale.' . $menu->name) }}</span>
-                                    @if (isset($menu->badge))
-                                            <?php $badgeClasses = 'badge rounded-pill badge-light-primary ms-auto me-1'; ?>
-                                        <span
-                                            class="{{ isset($menu->badgeClass) ? $menu->badgeClass : $badgeClasses }}">{{ $menu->badge }}</span>
-                                    @endif
-                                </a>
-                                @if (isset($menu->submenu))
-                                    @include('panels/submenu', ['menu' => $menu->submenu])
-                                @endif
-                            </li>
                         @endif
+                        <li
+                            class="nav-item {{ $custom_classes }} {{ Route::currentRouteName() === $menu->slug ? 'active' : '' }}">
+                            <a href="{{ isset($menu->url) ? url($menu->url) : 'javascript:void(0)' }}"
+                               class="d-flex align-items-center"
+                               target="{{ isset($menu->newTab) ? '_blank' : '_self' }}">
+                                <i data-feather="{{ $menu->icon }}"></i>
+                                <span class="menu-title text-truncate">{{ __('locale.' . $menu->name) }}</span>
+                                @if (isset($menu->badge))
+                                        <?php $badgeClasses = 'badge rounded-pill badge-light-primary ms-auto me-1'; ?>
+                                    <span
+                                        class="{{ isset($menu->badgeClass) ? $menu->badgeClass : $badgeClasses }}">{{ $menu->badge }}</span>
+                                @endif
+                            </a>
+                            @if (isset($menu->submenu))
+                                @include('panels/submenu', ['menu' => $menu->submenu])
+                            @endif
+                        </li>
                     @endif
                 @endforeach
             @endif
