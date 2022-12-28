98d0ca8bc6fbc9ad20638557e742f90fac548536
irvingviveros <irvingviveros@users.noreply.github.com>
2022-12-28 10:30:49 -0600

    chore: updated fillable fields

--- src/Infrastructure/User/Model/User.php
+++ src/Infrastructure/User/Model/User.php
@@ -20,9 +20,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'status',
+        'is_active',
+        'created_by',
+        'school_id'
     ];
 
     /**
