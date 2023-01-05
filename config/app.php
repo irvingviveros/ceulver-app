5e3f11f72f116bd783ed57d18ddd7ef09213857f
irvingviveros <irvingviveros@users.noreply.github.com>
2023-01-05 15:53:53 -0600

    chore: remove Luecano NumeroALetras provider

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
         Brian2694\Toastr\ToastrServiceProvider::class,
-        Luecano\NumeroALetras\NumeroALetras::class,
         /*
          * Package Service Providers...
          */
