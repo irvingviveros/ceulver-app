a273bf81c1f5a673b6924d2425da81e129365e73
irvingviveros <irvingviveros@users.noreply.github.com>
2023-01-06 13:01:34 -0600

    chore: accounting user now has access to students

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -82,6 +82,26 @@
                             </li>
                         @endif
 
+                        @if($menu->name === 'Students' && auth()->user()->hasAnyRole('accounting'))
+                            <li
+                                class="nav-item {{ $custom_classes }} {{ Route::currentRouteName() === $menu->slug ? 'active' : '' }}">
+                                <a href="{{ isset($menu->url) ? url($menu->url) : 'javascript:void(0)' }}"
+                                   class="d-flex align-items-center"
+                                   target="{{ isset($menu->newTab) ? '_blank' : '_self' }}">
+                                    <i data-feather="{{ $menu->icon }}"></i>
+                                    <span class="menu-title text-truncate">{{ __('locale.' . $menu->name) }}</span>
+                                    @if (isset($menu->badge))
+                                            <?php $badgeClasses = 'badge rounded-pill badge-light-primary ms-auto me-1'; ?>
+                                        <span
+                                            class="{{ isset($menu->badgeClass) ? $menu->badgeClass : $badgeClasses }}">{{ $menu->badge }}</span>
+                                    @endif
+                                </a>
+                                @if (isset($menu->submenu))
+                                    @include('panels/submenu', ['menu' => $menu->submenu])
+                                @endif
+                            </li>
+                        @endif
+
                         @if(auth()->user()->hasAnyRole('super-admin'))
                             <li
                                 class="nav-item {{ $custom_classes }} {{ Route::currentRouteName() === $menu->slug ? 'active' : '' }}">
