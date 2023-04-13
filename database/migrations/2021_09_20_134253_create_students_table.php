4096ccf5cd971675a31df4634c47065aac423f94
irvingviveros <irvingviveros@users.noreply.github.com>
2023-04-13 00:03:58 -0600

    fix: admin can edit students. The form has fewer restrictions.

--- database/migrations/2021_09_20_134253_create_students_table.php
+++ database/migrations/2021_09_20_134253_create_students_table.php
@@ -21,7 +21,7 @@ class CreateStudentsTable extends Migration
             $table->string('enrollment')->nullable(true); // Matrícula
             $table->string('admission_no', 20)->nullable(true); // Número de admisión (registro en línea)
             $table->date('admission_date')->nullable(true); // Fecha de admisión (registro en línea)
-            $table->string('payment_reference')->nullable(true); // Referencia de pago
+            $table->string('payment_reference')->nullable(true)->default('N/A'); // Referencia de pago
             $table->string('first_name', 50);  // Nombre(s)
             $table->string('paternal_surname', 25); // Apellido paterno
             $table->string('maternal_surname', 25)->nullable(true); // Apellido materno
@@ -29,8 +29,8 @@ class CreateStudentsTable extends Migration
             $table->integer('age')->nullable(true); // Edad //TODO: calcular automáticamente edad en base a birth_date
             $table->string('occupation', 100)->nullable(true); // Ocupación
             $table->string('nationality', 100)->nullable(true)->default('Mexicana'); // País
-            $table->string('address', 250)->nullable(true); // Dirección
-            $table->string('personal_email')->unique()->nullable(true); // Correo personal
+            $table->string('address', 250)->nullable(true)->default('N/A'); // Dirección
+            $table->string('personal_email')->unique()->nullable(true)->default('N/A'); // Correo personal
             $table->string('personal_phone', 17)->nullable(true); // Teléfono personal
             $table->string('marital_status')->nullable(true)->default('No aplica'); //Estado civil
             $table->string('sex', 20)->nullable(true); // SexEnum, Sexo
@@ -38,7 +38,7 @@ class CreateStudentsTable extends Migration
             $table->string('blood_group', 15)->nullable(true); // Grupo de sangre
             $table->string('allergies', 250)->nullable(true); // Alergias
             $table->string('ailments', 250)->nullable(true); // Padecimientos
-            $table->string('guardian_relationship')->nullable(false)->default('Otro'); // Relación con el padre/tutor
+            $table->string('guardian_relationship')->nullable()->default('Otro'); // Relación con el padre/tutor
             $table->timestamps();
 
             // Foreign key
